8990924e1cdd8175360aeeaf61c6c07390bd213b
Frank <f.neumann.berlin@googlemail.com>
2024-03-28 12:41:01 +0100

    mit landscape layout

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.tictactoe
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var currentPlayer = "X"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.tvStatus.text = getString(R.string.tv_status_start, currentPlayer)
 
     }
+
+    fun onClick(view: View) {}
 }
