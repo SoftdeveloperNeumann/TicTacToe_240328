73db2311e99afba96bae11e798cd36d7819221b5
Frank <f.neumann.berlin@googlemail.com>
2024-04-02 08:59:00 +0200

    mit Spielerwechsel und resetGame

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.tictactoe
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tictactoe.databinding.ActivityMainBinding
 
@@ -9,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     private var currentPlayer = "X"
+    lateinit var allFields: Array<TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +19,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.tvStatus.text = getString(R.string.tv_status_start, currentPlayer)
+        allFields = with(binding) {
+            arrayOf(f0, f1, f2, f3, f4, f5, f6, f7, f8)
+        }
 
     }
 
-    fun onClick(view: View) {}
+    fun onClick(view: View) {
+        val textview = view as TextView
+
+        if (textview.text == "") {
+            if (currentPlayer == "X") textview.setTextColor(Color.GREEN) else textview.setTextColor(
+                Color.RED
+            )
+            textview.text = currentPlayer
+
+            currentPlayer = if (currentPlayer == "X") "O" else "X"
+
+            if(allFields.all { it.text != "" }){
+                resetGame()
+            }
+        }
+
+    }
+
+    fun resetGame() {
+        allFields.forEach { it.text = "" }
+    }
 }
